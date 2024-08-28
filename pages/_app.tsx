da8f0ca61d6771f25908770915f24319d7d15d9f
Oleksii Knihin <alexyo2626@gmail.com>
2024-08-29 01:44:13 +0300

    change bugs with component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/global.css";
 import Layout from "../components/Layout/index";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-}
+};
+
+export default App;
